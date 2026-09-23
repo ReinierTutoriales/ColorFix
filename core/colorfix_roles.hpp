3683d01112a1ff3e8b38d1e0bc083980fad67530
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-23 07:54:03 -0400

    fix: make core portable and compiled

--- core/colorfix_roles.hpp
+++ core/colorfix_roles.hpp
@@ -11,11 +11,16 @@ constexpr COLORREF MapSystemColor(int index, COLORREF original) noexcept {
     case COLOR_BTNFACE:      return Rgb(45, 45, 45);
     case COLOR_BTNTEXT:      return Rgb(220, 220, 220);
     case COLOR_GRAYTEXT:     return Rgb(145, 145, 145);
-    case COLOR_3DFACE:       return Rgb(45, 45, 45);
     case COLOR_3DSHADOW:     return Rgb(65, 65, 65);
     case COLOR_3DHILIGHT:    return Rgb(85, 85, 85);
+    case COLOR_3DLIGHT:      return Rgb(72, 72, 72);
+    case COLOR_3DDKSHADOW:   return Rgb(24, 24, 24);
+    case COLOR_WINDOWFRAME:  return Rgb(64, 64, 64);
+    case COLOR_APPWORKSPACE: return Rgb(28, 28, 28);
+    case COLOR_SCROLLBAR:    return Rgb(52, 52, 52);
     case COLOR_MENU:         return Rgb(37, 37, 37);
     case COLOR_MENUTEXT:     return Rgb(220, 220, 220);
+    case COLOR_MENUBAR:      return Rgb(37, 37, 37);
     case COLOR_INFOBK:       return Rgb(48, 48, 48);
     case COLOR_INFOTEXT:     return Rgb(220, 220, 220);
     default:                 return original;
